0995272227e1d1689b09d4c5b7bb446ddb39d45c
thinkie72 <tyler.hinkie@menloschool.org>
2024-09-12 23:04:26 -0700

    Work Log Entry 3

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -4,6 +4,7 @@ Name: Tyler Hinkie
 |:-------------|:-------------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
 | September 11 |  8:10-8:40am  |        I wrote case for when hospital costs less or equal, in which case you just build all hospitals, and then wrote pseudocode for the algorithm for teh other part (lots of thinking and less code writing in this session). |
 | September 11 | 10:45-11:25am | I started attempting to use ArrayLists to sort, map, and categorize the clusters. In doing so, I could then just multiply the number of clusters by hospitals, then the number of roads by number of cities in the cluster - 1. |
+| September 12 |  10:30-11pm   |                                                                                  Began implementing my first attempt at an algorithm to solve the problem and find the minimum cost. Will update more on success in next entry. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -14,24 +14,47 @@ public class HighwaysAndHospitals {
 
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
         if (highwayCost >= hospitalCost) {
-            return (int) (hospitalCost * n);
+            return (long) hospitalCost * n;
         }
 
+        boolean[] hospitalAccess = new boolean[n + 1];
         int m = cities.length;
         int two = cities[0].length;
 
-        ArrayList<ArrayList<Integer>> map = new ArrayList<ArrayList<Integer>>();
-        Array
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < two; j++) {
-                map.add(new ArrayList<>());
-                map
+        ArrayList<Integer>[] map = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            map[i] = new ArrayList<>();
+        }
+
+        int start;
+        int end;
+
+        for (int[] road : cities) {
+            start = road[0];
+            end = road[1];
+            map[start].add(end);
+            map[end].add(start);
+        }
+
+        int hospitals = 0;
+        int roads = 0;
+
+        for (int i = 0; i < map.length; i++) {
+            if (!map[i].isEmpty() && !hospitalAccess[i]) {
+                hospitalAccess[i] = true;
+                hospitals++;
+                for (int j = 0; j < map[i].size(); j++) {
+                    hospitalAccess[map[i].get(j)] = true;
+                    roads++;
+                }
             }
         }
+
+        return
             // Traverse through array to map out roads
             // Make a hospital for each cluster
             // Make roads for each cluster to connect to the hospital
-        }
         return 0;
     }
 }
