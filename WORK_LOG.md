73b33b8f22581f4f2387be5c04061549a4462b7b
thinkie72 <tyler.hinkie@menloschool.org>
2024-09-12 23:52:10 -0700

    Work Log Entry 5

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -6,6 +6,7 @@ Name: Tyler Hinkie
 | September 11 | 10:45-11:25am | I started attempting to use ArrayLists to sort, map, and categorize the clusters. In doing so, I could then just multiply the number of clusters by hospitals, then the number of roads by number of cities in the cluster - 1. |
 | September 12 |  10:30-11pm   |                                                                                  Began implementing my first attempt at an algorithm to solve the problem and find the minimum cost. Will update more on success in next entry. |
 | September 12 |  11-11:30pm   |                                                 The former algorithm did not work, so I tried to use a queue instead to check for all of the neighbors like in a tree fashion, and it worked. Comments and style coming soon... |
+| September 12 | 11:30-11:50pm |                                                                                 Wrote comments to understand the process of my first attempt, especially for when I go back and make changes later before the final submission. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -14,18 +14,21 @@ import java.util.Queue;
 public class HighwaysAndHospitals {
 
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-        if (highwayCost >= hospitalCost) {
+        // Returns case from class where if hospitals cost <= highways then just build hospitals everywhere
+        if (highwayCost >= hospitalCost)
             return (long) hospitalCost * n;
-        }
 
+        // Array to hold whether or not a specific town has access to a hospital yet
         boolean[] hospitalAccess = new boolean[n + 1];
 
+        // Array of ArrayLists to map out all the cities and what towns they connect to
         ArrayList<Integer>[] map = new ArrayList[n + 1];
 
-        for (int i = 0; i < n + 1; i++) {
+        // Filler for-loop to initialize array
+        for (int i = 0; i < n + 1; i++)
             map[i] = new ArrayList<>();
-        }
 
+        // Variables for road entrances and exits
         int start;
         int end;
 
@@ -36,21 +39,26 @@ public class HighwaysAndHospitals {
             map[end].add(start);
         }
 
+        // Counter variables for later multiplication to find cost
         int hospitals = 0;
         int highways = 0;
+
         int town;
 
         for (int i = 1; i < n + 1; i++) {
+            // For any city that does not yet have hospital access, add a hospital
             if (!hospitalAccess[i]) {
                 hospitals++;
+                // Queue to continually make sure that the connecting roads to complete clusters have hospital access
                 Queue<Integer> q = new LinkedList<>();
                 q.add(i);
                 while (!q.isEmpty()) {
                     town = q.remove();
-                    if (!hospitalAccess[town]) {
+                    if (!hospitalAccess[town])
                         hospitalAccess[town] = true;
-                    }
                     for (int neighbor : map[town]) {
+                        // Builds highways for all the neighboring cities and then
+                        // adds each neighbor to the queue to look at their neighbors
                         if (!hospitalAccess[neighbor]) {
                             hospitalAccess[neighbor] = true;
                             highways++;
