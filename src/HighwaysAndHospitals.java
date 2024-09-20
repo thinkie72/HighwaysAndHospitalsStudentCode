7fceb0190f658bf51525488f55c72537a490e1b3
thinkie72 <tyler.hinkie@menloschool.org>
2024-09-19 23:09:20 -0700

    Work Log Entry 9

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -14,34 +14,55 @@ import java.util.Queue;
 
 public class HighwaysAndHospitals {
 
+    // Finds minimum cost to give hospital access to n cities given an array of roads between cities
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+        // Returns if case where the hospitals cost less, where the min cost is just build hospitals everywhere
         if (highwayCost >= hospitalCost) return (long) hospitalCost * n;
+        // Map to hold roots and weight of tree if a root
         int[] roots = new int[n + 1];
-        int a, b, x, temp;
+        // Sets array elements equal to -1 for weight balancing (seemed more code efficient than Arrays.fill())
+        for (int i = 1; i <= n; i++) {
+            roots[i] = -1;
+        }
+        // Variables for all the roots and nodes on either side of the edge
+        int start, end, startRoot, endRoot;
+        // Counter to determine the number of clusters in a count-itself fashion
         int clusters = n;
-        for (int i = 0; i < cities.length; i++) {
-            a = cities[i][0];
-            b = cities[i][1];
-            while (roots[a] > -1) a = roots[a];
-            while (roots[b] > -1) b = roots[b];
-            x = a;
-            while (roots[x] > -1) x = roots[x];
-            while (roots[a] > -1) {
-                temp = roots[a];
-                roots[a] = x;
-                a = temp;
+        for (int[] possibleHighway : cities) {
+            // Initializes start and end of edge
+            start = possibleHighway[0];
+            end = possibleHighway[1];
+
+            // Uses helper function to find the root of the start and end of the edge
+            startRoot = findRoot(start, roots);
+            endRoot = findRoot(end, roots);
+
+            // If they have the same root, then they don't need to be readded and combined
+            if (startRoot != endRoot) {
+                // One less node is its own root
+                clusters--;
+
+                // Adds smaller cluster onto the larger one, depending on which one is larger
+                if (roots[startRoot] < roots[endRoot]) {
+                    roots[endRoot] += roots[startRoot];
+                    roots[startRoot] = endRoot;
+                } else {
+                    roots[startRoot] += roots[endRoot];
+                    roots[endRoot] = startRoot;
+                }
             }
-            // Find roots, R and S
-//            X = order(R)
-//            Y = order(S)
-//            if (X > Y)
-//                root[S] = R
-//            else
-//                root[R] = S
         }
 
-
-
+        // Calculates minimum cost to give hospital access to all cities
         return (long) hospitalCost * clusters + (long) highwayCost * (n - clusters);
+    }
+
+    // Finds the roots for a given node and uses path compression to flatten tree
+    private static int findRoot(int x, int[] roots) {
+        // Returns x if it's its own root
+        if (roots[x] < 0) return x;
+        // Path compression via recursive step
+        roots[x] = findRoot(roots[x], roots);
+        return roots[x];
     }
 }
